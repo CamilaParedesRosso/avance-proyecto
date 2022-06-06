1151a198400a5f7c42203da22a4424d5260f2807
CamilaParedesRosso <99370302+CamilaParedesRosso@users.noreply.github.com>
2022-06-06 15:42:34 -0400

    Update Empresa.py

--- Empresa.py
+++ Empresa.py
@@ -1,21 +1,30 @@
 import os
 
 class Empresa:
-    def __init__(self,nombre,correo,telefono,direccion):
+    def __init__(self,usuario,contrasena,nombre,correo,telefono,direccion):
+        self.usuario=usuario
+        self.contrasena=contrasena
         self.nombre=nombre
         self.correo=correo
         self.telefono=telefono
         self.direccion=direccion
                 
-    def lista(self):
-        l=[self.nombre,self.correo,self.telefono,self.direccion]
-        return l
+    def datos(self):
+        dato=[self.usuario,self.contrasena,self.nombre,self.correo,self.telefono,self.direccion]
+        return dato
     
-    def perfil(self):
-        print(self.nombre)
-        print(f'Correo: {self.correo}')
-        print(f'Telefono: {self.telefono}')
-        print(f'Direccion: {self.direccion}')
+    def mostrar_datos(self):
+        print('Usuario: ',(self.correo))
+        print('Nombre de la Empresa: ',(self.telefono))
+        print('Correo: ',(self.correo))
+        print('Telefono: ',(self.telefono))
+        print('Direccion: ',(self.direccion))
+    
+    def registro_usuario(self):
+        u=input('Usuario: ')
+        self.usuario=u
+        co=input('Contraseña: ')
+        self.contrasena=co
         
     def registro(self):
         n=input('Nombre de la empresa: ')
@@ -26,7 +35,19 @@ class Empresa:
         self.telefono=t
         e=input('Direccion: ')
         self.edad=e
+
+    def Crear_archivo(self):
+        import pickle
+        archivo=open('Empresa','ab')
+        d=self.datos()
+        pickle.dump(d,archivo)
+        archivo.close()
         
+    def get_usuario(self):
+        return self.usuario
+    
+    def get_contrasena(self):
+        return self.contrasena
 
     def get_nombre(self):
         return self.nombre
