feccf43be77bbd7bbb1ef667ae822d4d5b1a2cbd
CamilaParedesRosso <99370302+CamilaParedesRosso@users.noreply.github.com>
2022-06-22 17:28:22 -0400

    Update Op_Publico.py

--- Op_Publico.py
+++ Op_Publico.py
@@ -1,20 +1,43 @@
 import os
-
-def Amigos(self):
-    print('mandar a amigos')
-def ConocerPersonas(self):
-    print('1. Mostrar lista de opciones')
-    print('2. Añadir una nueva opcion')
-    
-print('Deseas organizar una salida con: ')
-print('1. Amigos')
-print('2. Conocer nuevas personas')
-print('3. Salir')
-op=''
-while op!=3:
-    if op==1:
-        a=amigos()
-        empresas=empresa.lista()
-        
-    elif op==2:
-        c=ConocerPersonas()
+class publicacion_con_amigos:
+    def __init__(self):
+        self.lis=[]
+    def leer(self):
+        pub= "publicacion_con_amigos.txt"
+        if pub in os.listdir("."):
+            print("\nArchivo creado en la ruta: \n\n\t{0}/{1}".format(os.getcwd(),pub))
+        else:
+            archivo = open(pub, "x")
+            archivo.close()
+        archivo = open(pub, "r")
+        for linea in archivo:
+            self.lis.append(linea)
+        archivo.close()
+    def mostrar(self):
+        print(self.lis)
+    def añadir(self):
+        print("Ingresa los datos de la publicación: ")
+        usuario=str(input("Tu nombre: "))
+        lugar=str(input("Lugar de la reunión: "))
+        fecha=str(input("Fecha de la reunion: "))
+        publi=usuario+"\r"+lugar+"\r"+fecha+"\n"
+        self.lis.append(publi)
+    def modificarañadir(self):
+        ar = "publicaciones.txt"
+        archivo=open(ar,"w")
+        for i in self.lis:
+            archivo.write(i)
+        archivo.close()
+    def reiniciar(self):
+        self.lis=[]
+pub=publicacion()
+opcion=-1
+while opcion!="0":
+    print("Qué quieres hacer?\n\t1-Crear una publicacion\n\t0-Cerrar")
+    opcion=input("")
+    if opcion=="1":
+        pub.leer()
+        pub.mostrar()
+        pub.añadir()
+        pub.modificarañadir()
+        pub.reiniciar()
